db90571fb4a43bbd8c974ff81e8e2f3478bb1e2a
Konrad Malawski <konrad.malawski@project13.pl>
2011-04-16 12:58:51 +0200

    zaczynam prace nad cannonem, bede strzelal

--- js/Cannon.js
+++ js/Cannon.js
@@ -7,10 +7,33 @@
  */
 
 var Cannon = Class.extend({
+
     power: 15,
 
+    x: 30,
+    y: 30,
 
-    fire: function(){
+    currentBrowser: null,
 
+    browsers: [
+
+    ],
+
+    init: function() {
+
+    },
+
+    fire: function() {
+        mousePVec = new b2Vec2(this.x, this.y);
+        var aabb = new b2AABB();
+
+        aabb.lowerBound.Set(this.y - 0.001, this.y - 0.001);
+        aabb.upperBound.Set(this.y + 0.001, this.y + 0.001);
+
+        // Query the world for overlapping shapes.
+
+        selectedBody = null;
+        world.QueryAABB(getBodyCB, aabb);
+        return selectedBody;
     }
 });
